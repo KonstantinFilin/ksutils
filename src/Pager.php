477473206df93094cd6ab8bdd84f99bc656bd216
Konstantin S. Filin <info@kfilin.ru>
2013-10-09 02:30:31 +0400

    Added apidoc for \KsUtils\Pager class

--- src/Pager.php
+++ src/Pager.php
@@ -1,68 +1,135 @@
 <?php
 
-    namespace KsUtils;
+namespace KsUtils;
 
-    class Pager
+/**
+ * Simple pagination model for abstract list
+ */
+class Pager
+{
+    /**
+     * Total items on all pages
+     * @var int
+     */
+    protected $itemsTotal;
+
+    /**
+     * Items of current page
+     * @var array
+     */
+    protected $items;
+
+    /**
+     * Total number of pages
+     * @var int
+     */
+    protected $pagesTotal;
+
+    /**
+     * Current page num
+     * @var int
+     */
+    protected $curPage;
+
+    /**
+     * Maximum number of items on page
+     * @var int
+     */
+    protected $onpage;
+
+    function __construct()
     {
-        protected $itemsTotal;
-        protected $items;
-        protected $pagesTotal;
-        protected $curPage;
-        protected $onpage;
-
-        function __construct()
-        {
-            $this->curPage = 1;
-            $this->pagesTotal = 1;
-        }
-
-        public function getOnpage()
-        {
-            return $this->onpage;
-        }
-
-        public function setOnpage($onpage)
-        {
-            $this->onpage = $onpage;
-        }
-
-        public function getItemsTotal()
-        {
-            return $this->itemsTotal;
-        }
-
-        public function getItems()
-        {
-            return $this->items;
-        }
-
-        public function getPagesTotal()
-        {
-            return $this->pagesTotal;
-        }
-
-        public function getCurPage()
-        {
-            return $this->curPage;
-        }
-
-        public function setItemsTotal($itemsTotal)
-        {
-            $this->itemsTotal = $itemsTotal;
-        }
-
-        public function setItems($items)
-        {
-            $this->items = $items;
-        }
-
-        public function setPagesTotal($pagesTotal)
-        {
-            $this->pagesTotal = $pagesTotal;
-        }
-
-        public function setCurPage($curPage)
-        {
-            $this->curPage = $curPage;
-        }
+        $this->curPage = 1;
+        $this->pagesTotal = 1;
     }
+
+    /**
+     * Returns maximum number of items on page
+     * @return int
+     */
+    public function getOnpage()
+    {
+        return $this->onpage;
+    }
+
+    /**
+     * Sets maximum number of items on page
+     * @param int $onpage
+     */
+    public function setOnpage($onpage)
+    {
+        $this->onpage = $onpage;
+    }
+
+    /**
+     * Returns total items amount on all pages
+     * @return int
+     */
+    public function getItemsTotal()
+    {
+        return $this->itemsTotal;
+    }
+
+    /**
+     * Returns array of items
+     * @return array
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Returns total number of pages
+     * @return int
+     */
+    public function getPagesTotal()
+    {
+        return $this->pagesTotal;
+    }
+
+    /**
+     * Returns current page number
+     * @return int
+     */
+    public function getCurPage()
+    {
+        return $this->curPage;
+    }
+
+    /**
+     * Sets total items amount
+     * @param int $itemsTotal Total items amount
+     */
+    public function setItemsTotal($itemsTotal)
+    {
+        $this->itemsTotal = $itemsTotal;
+    }
+
+    /**
+     * Sets array of items
+     * @param array $items Array of items
+     */
+    public function setItems($items)
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * Sets total number of pages
+     * @param int $pagesTotal Total number of pages
+     */
+    public function setPagesTotal($pagesTotal)
+    {
+        $this->pagesTotal = $pagesTotal;
+    }
+
+    /**
+     * Sets current page number
+     * @param int $curPage Current page number
+     */
+    public function setCurPage($curPage)
+    {
+        $this->curPage = $curPage;
+    }
+}
