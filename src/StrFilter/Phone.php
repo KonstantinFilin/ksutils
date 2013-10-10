e52d1cb8ac80368242c712c5c55b0c809e4d6316
Konstantin S. Filin <info@kfilin.ru>
2013-10-10 11:33:32 +0400

    Added tests for  \KsUtils\StrFilter package

--- src/StrFilter/Phone.php
+++ src/StrFilter/Phone.php
@@ -17,12 +17,14 @@ class Phone implements \KsUtils\StrFilter
      */
     public function filter($str)
     {
-        $str = preg_replace("|[^\D]|", "", $str);
+        $str = preg_replace("|[\D+]|", "", $str);
 
         if (strlen($str) == 11 && substr($str, 0, 1) == 7) {
             $str = substr($str, 1);
         }
 
-        return $str;
+        $len2 = strlen($str);
+        
+        return $len2 == 10 ? $str : "";
     }
 }
