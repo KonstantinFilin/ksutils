c9d47d37669f335a7058c4806a0f0ce5502b2762
Konstantin S. Filin <info@kfilin.ru>
2013-10-11 12:09:14 +0400

    Changes in \KsUtils\Validator\Date class: * Added $errorMessage variable definition moved from method to contructor * Apidoc added

--- src/Validator/Date.php
+++ src/Validator/Date.php
@@ -3,31 +3,31 @@
 namespace KsUtils\Validator;
 
 /**
- * Description of Date
- *
- * @author kostya
+ * Validator for a date string in different format
  */
 class Date extends \KsUtils\Validator
 {
+    /**
+     * Format of the date
+     * @var string
+     */
     protected $format;
 
+    /**
+     * @param string $format Format of the date
+     */
     public function __construct($format = "Y-m-d")
     {
         $this->format = $format;
+        $this->errorMessage = "Wrong date: %s";
     }
 
+    /**
+     * @inheritdoc
+     */
     public function check($value)
     {
-        /*echo "<pre>";
-        var_dump($value);
-        die;*/
         $dtObj = date_create_from_format($this->format, $value);
-
         return $dtObj !== false;
-    }
-
-    public function getErrorMessage($errValue)
-    {
-        return "Неверный формат даты";
     }
 }
