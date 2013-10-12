ab2517a0cdee8613573afc1c08949be9e7ff4eda
Konstantin S. Filin <info@kfilin.ru>
2013-10-12 14:52:18 +0400

    Added apidoc for \KsUtils\Validator\StrLenRange class

--- src/Validator/StrLenRange.php
+++ src/Validator/StrLenRange.php
@@ -3,30 +3,37 @@
 namespace KsUtils\Validator;
 
 /**
- * Description of StrLenRange
- *
- * @author kostya
+ * String range validator
  */
 class StrLenRange extends \KsUtils\Validator
 {
+    /**
+     * Minimum string length
+     * @var int
+     */
     protected $min;
+
+    /**
+     * Maximum string length
+     * @var int
+     */
     protected $max;
 
     public function __construct($min, $max)
     {
         $this->min = $min;
         $this->max = $max;
-    }
 
-    public function getErrorMessage($errValue)
-    {
-        return sprintf(
-            "Длина строки должна быть от %u до %u символов",
+        $this->error = sprintf(
+            "String length must be  between %d and %d, checked value: %%s",
             $this->min,
             $this->max
         );
     }
 
+    /**
+     * @inheritdoc
+     */
     public function check($value)
     {
         $len = strlen($value);
